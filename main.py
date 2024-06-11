bb71c096be42eb9cc7d977c23bea0f69ae4c7920
Michael XG <michaelxg@gmail.com>
2024-06-10 22:06:19 -0300

    Ajutes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import pyodbc
 from fastapi import FastAPI, HTTPException
 import uvicorn
-from models.Usuarios import Usuarios
+from models.Usuarios import UpdateUsuarios, Usuarios
 from http import HTTPStatus
 import pandas as pd
 from Controllers.PadraoController import *
@@ -21,9 +21,8 @@ def get_conn():
         print("Erro ao conectar ao banco de dados 01:", e)
         return None
 
-@app.get("/UsuariosAll")
-def get_Usuarios():
-    usuarios = []
+@app.get("/UsuariosAll", status_code=HTTPStatus.OK, tags="Usuários")
+def get_All_Usuarios():
     conn = get_conn()
     if conn is None:
         raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados 02.")
@@ -69,43 +68,48 @@ def get_Usuarios():
             else:
                 return None
             
-@app.get("/Usuarios/{CodigoUsuario}")
+@app.get("/Usuarios/{CodigoUsuario}", status_code=HTTPStatus.OK, tags="Usuários")
 def get_User_CodigoUsuario(CodigoUsuario: int):
-    conn = get_conn()
-    if conn is None:
-        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados.")
-    
-    with conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT CodigoUsuario, CodigoEmpresa, dbo.fn_Descriptografa(NomeUsuario) AS NomeUsuario \
-                              , dbo.fn_Descriptografa(Apelido) AS Apelido, dbo.fn_Descriptografa(Password) AS Password \
-                              , dbo.fn_Descriptografa(CPF) AS CPF, dbo.fn_Descriptografa(Email) as Email, dbo.fn_Descriptografa(Telefone) as Telefone \
-                              , dbo.fn_Descriptografa(Celular) as Celular, Ativo, CodigoGrupoUsuario, InseridoPor, InseridoEm, ModificadoPor, ModificadoEm  \
-                           FROM Usuario WITH(NOLOCK) \
-                          WHERE CodigoUsuario = ?", CodigoUsuario)
+    try:
+        conn = get_conn()
+        if conn is None:
+            raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados.")
+        
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT CodigoUsuario, CodigoEmpresa, dbo.fn_Descriptografa(NomeUsuario) AS NomeUsuario \
+                                  , dbo.fn_Descriptografa(Apelido) AS Apelido, dbo.fn_Descriptografa(Password) AS Password \
+                                  , dbo.fn_Descriptografa(CPF) AS CPF, dbo.fn_Descriptografa(Email) as Email, dbo.fn_Descriptografa(Telefone) as Telefone \
+                                  , dbo.fn_Descriptografa(Celular) as Celular, Ativo, CodigoGrupoUsuario, InseridoPor, InseridoEm, ModificadoPor, ModificadoEm  \
+                               FROM Usuario WITH(NOLOCK) \
+                              WHERE CodigoUsuario = ?", CodigoUsuario)
 
-        usuario = cursor.fetchone()
-        if usuario is None:
-            raise HTTPException(status_code=404, detail="Usuário não encontrado.")
-        return {
-            "CodigoUsuario": usuario.CodigoUsuario,
-            "CodigoEmpresa": usuario.CodigoEmpresa,
-            "NomeUsuario": usuario.NomeUsuario,
-            "Apelido": usuario.Apelido,
-            "Password": usuario.Password,
-            "CPF": usuario.CPF,
-            "Email": usuario.Email,
-            "Telefone": usuario.Telefone,
-            "Celular": usuario.Celular,
-            "Ativo": usuario.Ativo,
-            "CodigoGrupoUsuario": usuario.CodigoGrupoUsuario,
-            "InseridoPor": usuario.InseridoPor,
-            "InseridoEm": usuario.InseridoEm,
-            "ModificadoPor": usuario.ModificadoPor,
-            "ModificadoEm": usuario.ModificadoEm
-        }
+            usuario = cursor.fetchone()
+            if usuario is None:
+                raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+            return {
+                "CodigoUsuario": usuario.CodigoUsuario,
+                "CodigoEmpresa": usuario.CodigoEmpresa,
+                "NomeUsuario": usuario.NomeUsuario,
+                "Apelido": usuario.Apelido,
+                "Password": usuario.Password,
+                "CPF": usuario.CPF,
+                "Email": usuario.Email,
+                "Telefone": usuario.Telefone,
+                "Celular": usuario.Celular,
+                "Ativo": usuario.Ativo,
+                "CodigoGrupoUsuario": usuario.CodigoGrupoUsuario,
+                "InseridoPor": usuario.InseridoPor,
+                "InseridoEm": usuario.InseridoEm,
+                "ModificadoPor": usuario.ModificadoPor,
+                "ModificadoEm": usuario.ModificadoEm
+            }
+    except Exception as e:
+        # Em caso de exceção
+        raise HTTPException(status_code=500, detail=f"Erro ao obter usuário do banco de dados: {str(e)}")
 
-@app.post("/Usuarios_post", status_code=HTTPStatus.CREATED)
+
+@app.post("/Usuarios_post", status_code=HTTPStatus.CREATED, tags="Usuários")
 def create_user(usuario: Usuarios):
     conn = get_conn()
     if conn is None:
@@ -119,7 +123,7 @@ def create_user(usuario: Usuarios):
         
         cursor.execute("""
             INSERT INTO Usuario (CodigoEmpresa, NomeUsuario, Apelido, Password, CPF, Email, Telefone, Celular, Ativo, CodigoGrupoUsuario, InseridoPor, InseridoEm, ModificadoPor, ModificadoEm) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            VALUES (?, dbo.fn_Criptografa(?), dbo.fn_Criptografa(?), dbo.fn_Criptografa(?), dbo.fn_Criptografa(?), dbo.fn_Criptografa(?), dbo.fn_Criptografa(?), dbo.fn_Criptografa(?), ?, ?, ?, ?, ?, ?)
         """, usuario_dict["CodigoEmpresa"], usuario_dict["NomeUsuario"], usuario_dict["Apelido"], usuario_dict["Password"], 
              usuario_dict["CPF"], usuario_dict["Email"], usuario_dict["Telefone"], usuario_dict["Celular"], 
              usuario_dict["Ativo"], usuario_dict["CodigoGrupoUsuario"], usuario_dict["InseridoPor"], 
@@ -136,3 +140,122 @@ def create_user(usuario: Usuarios):
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
+
+
+# {
+#     "CodigoEmpresa": 1,
+#     "NomeUsuario": "Maria",
+#     "Apelido": "Maria123",
+#     "Password": "senha123",
+#     "CPF": "01254785454",
+#     "Email": "maria@example.com",
+#     "Telefone": "123456789",
+#     "Celular": "987654321",
+#     "Ativo": true,
+#     "CodigoGrupoUsuario": 1,
+#     "InseridoPor": 1,
+#     "InseridoEm": "2024-06-10T10:00:00",
+#     "ModificadoPor": null,
+#     "ModificadoEm": null
+# }
+
+@app.put("/Usuarios/{CodigoUsuario}", status_code=HTTPStatus.OK, tags="Usuários")
+def update_user(CodigoUsuario: int, usuario: UpdateUsuarios):
+    conn = get_conn()
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados.")
+    
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT CodigoUsuario, CodigoEmpresa, dbo.fn_Descriptografa(NomeUsuario) AS NomeUsuario,
+                   dbo.fn_Descriptografa(Apelido) AS Apelido, dbo.fn_Descriptografa(Password) AS Password,
+                   dbo.fn_Descriptografa(CPF) AS CPF, dbo.fn_Descriptografa(Email) AS Email,
+                   dbo.fn_Descriptografa(Telefone) AS Telefone, dbo.fn_Descriptografa(Celular) AS Celular,
+                   Ativo, CodigoGrupoUsuario, InseridoPor, InseridoEm, ModificadoPor, ModificadoEm
+            FROM Usuario WITH(NOLOCK)
+            WHERE CodigoUsuario = ?
+        """, CodigoUsuario)
+        existing_user = cursor.fetchone()
+        
+        if not existing_user:
+            raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+        
+        # Convertendo possíveis NaNs para None
+        usuario_dict = usuario.dict(exclude_unset=True)
+        usuario_dict = {k: (None if pd.isna(v) else v) for k, v in usuario_dict.items()}
+        
+        # Processamento dos campos a serem criptografados
+        encrypt_columns = ["NomeUsuario", "Apelido", "Password", "CPF", "Email", "Telefone", "Celular"]
+        for column in encrypt_columns:
+            if column in usuario_dict and usuario_dict[column] is not None:
+                usuario_dict[column] = f"dbo.fn_Criptografa('{usuario_dict[column]}')"
+        
+        # Atualiza apenas os campos fornecidos na requisição
+        set_clause = ", ".join(f"{key} = {value}" if key in encrypt_columns and value is not None else f"{key} = ?" for key, value in usuario_dict.items())
+        params = [value for key, value in usuario_dict.items() if key not in encrypt_columns or value is None]
+        params.append(CodigoUsuario)
+        
+        
+        print('set_clause', set_clause)
+        cursor.execute(f"""
+            UPDATE Usuario
+            SET {set_clause}
+            WHERE CodigoUsuario = ?
+        """, params)
+        
+        conn.commit()
+
+        cursor.execute("""
+            SELECT CodigoUsuario, CodigoEmpresa, dbo.fn_Descriptografa(NomeUsuario) AS NomeUsuario,
+                   dbo.fn_Descriptografa(Apelido) AS Apelido, dbo.fn_Descriptografa(Password) AS Password,
+                   dbo.fn_Descriptografa(CPF) AS CPF, dbo.fn_Descriptografa(Email) AS Email,
+                   dbo.fn_Descriptografa(Telefone) AS Telefone, dbo.fn_Descriptografa(Celular) AS Celular,
+                   Ativo, CodigoGrupoUsuario, InseridoPor, InseridoEm, ModificadoPor, ModificadoEm
+            FROM Usuario WITH(NOLOCK)
+            WHERE CodigoUsuario = ?
+        """, CodigoUsuario)
+        updated_user = cursor.fetchone()
+        
+    except Exception as e:
+        print("Erro:", str(e))
+        conn.rollback()
+        raise HTTPException(status_code=500, detail="Erro ao atualizar usuário no banco de dados.")
+    finally:
+        conn.close()
+
+    return updated_user
+
+@app.delete("/Usuarios/{CodigoUsuario}", status_code=HTTPStatus.OK, tags="Usuários")
+def delete_user(CodigoUsuario: int):
+    conn = get_conn()
+    if conn is None:
+        raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados.")
+    
+    
+    return {"message": "Usuário deletado com sucesso!"}
+
+# @app.delete("/Usuarios/{CodigoUsuario}")
+# def delete_user(CodigoUsuario: int):
+#     conn = get_conn()
+#     if conn is None:
+#         raise HTTPException(status_code=500, detail="Erro ao conectar ao banco de dados.")
+    
+#     try:
+#         cursor = conn.cursor()
+#         cursor.execute("""SELECT CodigoUsuario FROM Usuario WITH(NOLOCK) WHERE CodigoUsuario = ? """, CodigoUsuario)
+#         existing_user = cursor.fetchone()
+        
+#         if not existing_user:
+#             raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+        
+#         cursor.execute("""DELETE FROM Usuario WHERE CodigoUsuario = ? """, CodigoUsuario)
+#         conn.commit()
+#     except Exception as e:
+#         print("Erro:", str(e))
+#         conn.rollback()
+#         raise HTTPException(status_code=500, detail="Erro ao deletar usuário do banco de dados.")
+#     finally:
+#         conn.close()
+
+#     return {"message": "Usuário deletado com sucesso!"}
--- models/Usuarios.py
+++ models/Usuarios.py
@@ -1,18 +1,52 @@
+import pandas as pd
+from pydantic import BaseModel, conint, constr, validator, Field
+from typing import Optional
 from datetime import datetime
-from pydantic import BaseModel, Field
 
 class Usuarios(BaseModel):
-    CodigoEmpresa      : int
-    NomeUsuario        : str | None = None
-    Apelido            : str | None = None
-    Password           : str | None = None
-    CPF                : str | None = None
-    Email              : str | None = None
-    Telefone           : str | None = None
-    Celular            : str | None = None
-    Ativo              : bool
-    CodigoGrupoUsuario : int
-    InseridoPor        : int
-    InseridoEm         : datetime 
-    ModificadoPor      : int
-    ModificadoEm       : datetime | None = None
+    CodigoEmpresa       : conint(ge=1) # type: ignore
+    NomeUsuario         : constr(strip_whitespace=True, min_length=1, max_length=255) # type: ignore
+    Apelido             : constr(strip_whitespace=True, min_length=1, max_length=40) # type: ignore
+    Password            : constr(strip_whitespace=True, min_length=6, max_length=14) # type: ignore
+    CPF                 : constr(strip_whitespace=True, min_length=11, max_length=11) # type: ignore
+    Email               : constr(strip_whitespace=True, max_length=80) # type: ignore
+    Telefone            : Optional[constr(strip_whitespace=True, min_length=10, max_length=10)] = None # type: ignore
+    Celular             : constr(strip_whitespace=True, min_length=11, max_length=11) # type: ignore
+    Ativo               : bool
+    CodigoGrupoUsuario  : conint(ge=1) # type: ignore
+    InseridoPor         : conint(ge=1) # type: ignore
+    InseridoEm          : datetime
+    ModificadoPor       : Optional[conint(ge=1)] = None # type: ignore
+    ModificadoEm        : Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
+
+    @validator("CPF")
+    def validate_cpf(cls, value):
+        if not value.isdigit() or len(value) != 11:
+            raise ValueError("CPF deve conter exatamente 11 dígitos.")
+        return value
+
+class UpdateUsuarios(BaseModel):
+    CodigoEmpresa       : Optional[conint(ge=1)] # type: ignore
+    NomeUsuario         : Optional[constr(strip_whitespace=True, min_length=1, max_length=255)] # type: ignore
+    Apelido             : Optional[constr(strip_whitespace=True, min_length=1, max_length=40)] # type: ignore
+    Password            : Optional[constr(strip_whitespace=True, min_length=6, max_length=14)] # type: ignore
+    CPF                 : Optional[constr(strip_whitespace=True, min_length=11, max_length=11)] # type: ignore
+    Email               : Optional[constr(strip_whitespace=True, max_length=80)] # type: ignore
+    Telefone            : Optional[constr(strip_whitespace=True, min_length=10, max_length=10)] = None # type: ignore
+    Celular             : Optional[constr(strip_whitespace=True, min_length=11, max_length=11)] # type: ignore
+    Ativo               : Optional[bool]
+    CodigoGrupoUsuario  : Optional[conint(ge=1)] # type: ignore
+    ModificadoPor       : conint(ge=1) # type: ignore
+    ModificadoEm        : datetime
+
+    class Config:
+        orm_mode = True
+
+    @validator("CPF", always=True)
+    def validate_cpf(cls, value):
+        if value and (not value.isdigit() or len(value) != 11):
+            raise ValueError("CPF deve conter exatamente 11 dígitos.")
+        return value
